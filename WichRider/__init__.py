3a4689a6cd55169042a367d03fdbdd8088e39a5a
Bxkb83 <158228087+Bxkb83@users.noreply.github.com>
2024-06-23 03:51:38 +0300

    Delete WichRider directory

--- WichRider/__init__.py
+++
@@ -1,8 +0,0 @@
-# @FnarkBey - < https://t.me/FnarkBey >
-# Copyright (C) 2022
-# Tüm hakları saklıdır.
-#
-# Bu dosya, < https://github.com/Bxkb83-github/Botaf8 > parçasıdır.
-# Lütfen GNU Affero Genel Kamu Lisansını okuyun;
-# < https://github.com/Bxkb83/Botaf8/blob/main/LICENSE >
-# ================================================================
